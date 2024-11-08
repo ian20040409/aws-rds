a846aede19903edccd5723198d08e743e4b03967
You <78723629+ian20040409@users.noreply.github.com>
2024-11-09 00:16:58 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
 <?php include "../inc/dbinfo.inc"; ?>
-<head>
+<<head>
+		<title>🏠Home Page</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
 <meta charset="utf-8" />
   <link rel="stylesheet" href="/photo_post/st/css/main.css" />
 							<link rel="icon" href="images/Sports_Mode_icon-icons.com_54137.ico">
